383b5e6b120f117d885984e857286e53c3dc76ab
azdoolittle <drewdoolittle@gmail.com>
2020-04-16 18:14:38 -0400

    added square and combine lines

--- src/Calculations.java
+++ src/Calculations.java
@@ -17,18 +17,33 @@ import java.util.Scanner;
 
 public class Calculations {
     public static void main(String[] args) {
-        int num1, num2, sum, product;
-        Scanner inputNum = new Scanner(System.in);
+        int num1, num2, num3, sum, product, square;
+        String inputText1, inputText2, outputText;
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Enter a number: ");
-        num1 = inputNum.nextInt();
+        num1 = input.nextInt();
         System.out.println("Enter another number: ");
-        num2 = inputNum.nextInt();
+        num2 = input.nextInt();
+        System.out.println("Enter another number: ");
+        num3 = input.nextInt();
+        input.nextLine();
+        System.out.println("Enter some words: ");
+        inputText1 = input.nextLine();
+        System.out.println("Enter some more words: ");
+        inputText2 = input.nextLine();
+
 
         sum = num1 + num2;
         product = num1 * num2;
+        square = num3 * num3;
+        outputText = inputText1 + " " + inputText2;
+
 
         System.out.println("Sum: " + sum);
         System.out.println("Product: " + product);
+        System.out.println("Square: " + square);
+        System.out.printf("Combined sentences: %s", outputText);
+
     }
 }
